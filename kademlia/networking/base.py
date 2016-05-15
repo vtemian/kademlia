6522c7e4ad69b304f0cfbeb70b54094bb03996d4
Vlad Temian <vladtemian@gmail.com>
2016-05-15 17:04:05 +0300

    Handle message passing in base server.

--- kademlia/networking/base.py
+++ kademlia/networking/base.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+VALID_JOBS_TYPES = ["store", "find_node", "find_value", "ping"]
+
 
 class Base(metaclass=ABCMeta):
     def __init__(self, node):
@@ -22,13 +24,29 @@ class BaseServer(Base):
     def receive(self):
         raise NotImplemented("receive method needs to be implemented on BaseClient")
 
-    def handle_receive(self, data):
-        if data["type"] == "store":
-            self.store(data["job_description"])
-        elif data["type"] == "":
-            pass
+    def handle_receive(self, job):
+        """
+            Handle different types of jobs (messsages comming from other nodes).
+        """
+
+        if job["type"] in self.__dict__ and callable(self.__dict__[job["type"]]):
+            return self.__dict__[job["job_type"]](job["data"])
+
+        raise ValueError("Invalid job type %s. Not in %s" %
+                         (job["type"], ",".join(VALID_JOBS_TYPES)))
 
     @abstractmethod
     def store(self, job):
         raise NotImplemented("store method needs to be implemented on BaseServer")
 
+    @abstractmethod
+    def find_node(self, job):
+        raise NotImplemented("find_node method needs to be implemented on BaseServer")
+
+    @abstractmethod
+    def find_value(self, job):
+        raise NotImplemented("find_value method needs to be implemented on BaseServer")
+
+    @abstractmethod
+    def ping(self, job):
+        raise NotImplemented("ping method needs to be implemented on BaseServer")
